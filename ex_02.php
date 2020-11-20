3f9f61e947927e0fda018deac73c95f1c28c2076
MelanieLuz <74184074+MelanieLuz@users.noreply.github.com>
2020-11-20 11:35:49 +0100

    Update

---
+++ ex_02.php
@@ -0,0 +1,16 @@
+<?php
+function min_len_array($key1, $key2)
+{
+array($key1, $key2);
+
+if ($key1 < $key2);
+return $key1;
+
+if ($key1 == $key2);
+return FALSE;
+
+if ($key2 > $key1);
+return FALSE;
+}
+
+var_dump(min_len_array(8, 8));
