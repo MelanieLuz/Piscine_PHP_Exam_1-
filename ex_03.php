20443d18fe86a2f4d788f8c411153db93a81e7a5
MelanieLuz <74184074+MelanieLuz@users.noreply.github.com>
2020-11-20 11:42:54 +0100

    Update

---
+++ ex_03.php
@@ -0,0 +1,15 @@
+<?php
+function resum_join_str ($chaine1, $chaine2)
+{
+
+if ($chaine1)
+return FALSE;
+
+if ($chaine1 < 3);
+return ".";
+
+if ($chaine1 > 3);
+return $chaine1 + $chaine2;
+}
+
+var_dump(resum_join_str(0, 4));
